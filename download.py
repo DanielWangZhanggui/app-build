21c9b1e3f39fed170fbae0eb4e5095e5227363ae
daniel <daniel@zhanggui.wang-pc>
2018-01-10 15:48:43 +0800

    a

--- download.py
+++ download.py
@@ -2,7 +2,16 @@ import oci
 import os
 import requests
 from oci.signer import Signer
+# from oci.config import from_file
 
+# config = oci.config.from_file()
+# auth = Signer(
+#     tenancy=config['tenancy'],
+#     user=config['user'],
+#     fingerprint=config['fingerprint'],
+#     private_key_file_location=config['key_file']
+    # pass_phrase=config['pass_phrase']
+# )
 
 lines = os.environ['PRIVATE_KEY'].split("\\n")
 file = open("oci_api_key.pem", "w")
@@ -16,17 +25,19 @@ auth = Signer(
     private_key_file_location='oci_api_key.pem'
 )
 url = os.environ['URL']
-# url = 'https://objectstorage.us-ashburn-1.oraclecloud.com/n/zte/b/artifacts-apps/o/cpu-load-0.0.1-SNAPSHOT.war'
+
+
+# url = 'https://objectstorage.us-ashburn-1.oraclecloud.com/n/ZTE/b/artifacts-apps/o/cpu-load-0.0.1-SNAPSHOT.war'
 
 
 local_filename = url.split('/')[-1]
-r = requests.get(url, stream=True)
+r = requests.get(url, stream=True, auth=auth)
 with open(local_filename, 'wb') as f:
     for chunk in r.iter_content(chunk_size=1024): 
         if chunk:
             f.write(chunk)
 
-response = requests.get(url, auth=auth)
+# response = requests.get(url, auth=auth)
 os.remove("oci_api_key.pem")
 
 ##upload file to Object Storage Service via : oci os object put -ns zte  -bn artifacts-apps --name kubeconfig --file /home/daniel/.kube/config
